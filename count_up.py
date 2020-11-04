e35353ec534b8f0b3720d3310d58fff65748456d
andresn <andres.narva@gmail.com>
2020-11-03 23:50:33 -0800

    code complete question #2

--- count_up.py
+++ count_up.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 #%%
 # this is a python script template
 # this next line will download the file using curl
@@ -48,7 +47,6 @@ with gzip.open(gff,"rt") as fh:
                 total_length_of_genes += int(row[4]) - int(row[3])
             i += 1
 
-total_length_of_genome = 0
 with gzip.open(fasta,"rt") as fh:
     seqs = dict(aspairs(fh))
     first_codon = {}
@@ -60,30 +58,23 @@ with gzip.open(fasta,"rt") as fh:
     for seqname in seqs:
         total_length_of_genome = len(seqs[seqname])
 
-perecent_of_genome_which_is_encoding = '{:.2%}'.format(total_length_of_genes / total_length_of_genome)
-total_length_of_genome = '{:,}'.format(total_length_of_genome)
+answers = [
+    '2. Total number of genes: {:,}'.format(number_of_genes),
+    '3. Total length of genes: {:,}'.format(total_length_of_genes),
+    '4. Total length of genome: {:,}'.format(total_length_of_genome),
+    '5. Perecent of genome which is coding: {:.2%}'.format(total_length_of_genes / total_length_of_genome)
+]
 
-number_of_genes = '{:,}'.format(number_of_genes)
-total_length_of_genes = '{:,}'.format(total_length_of_genes)
-
-print('2. Total number of genes:' + number_of_genes)
-print('3. Total length of genes:' + total_length_of_genes)
-print('4. Total length of genome: ' + total_length_of_genome)
-print('5. Perecent of genome which is coding: ' + perecent_of_genome_which_is_encoding)
+for answer in answers:
+    print(answer)
 
 if ENABLE_TESTS:
-    # Bash test: 
-    # zcat < Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz | cut -f 3,3 | grep -v ### | grep -v #! | grep -v ## | grep gene | sort | uniq -c
-    assert number_of_genes == '4,142', 'number_of_genes = ' + str(number_of_genes) + ', expected = 4,142'
-    # Bash test:
-    # zcat < Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz | awk '!/^#+/' |  awk '$3 ~ /^gene$/ {print $0}' | awk '{sum += $5-$4} END {print sum}'
-    assert total_length_of_genes == '3,944,686', 'total_length_of_genes = ' + str(total_length_of_genes) + ', expected = 3,944,686'
-
-    assert total_length_of_genome == '4,641,652', 'total_length_of_genome = ' + str(total_length_of_genome) + ', expected = 4,641,652'
-    assert perecent_of_genome_which_is_encoding == '84.98%', 'perecent_of_genome_which_is_encoding = ' + perecent_of_genome_which_is_encoding + ', expected = 84.98%'
-
-# Bash test:
-# zcat < Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz | tail -n 77361 | tr -d '\n' | wc -c
-
+    # Bash test: zcat < Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz | cut -f 3,3 | grep -v ### | grep -v #! | grep -v ## | grep gene | sort | uniq -c
+    assert answers[0] == '2. Total number of genes: 4,142', 'Answer 1 = ' + answers[0] + ', expected = 2. Total number of genes: 4,142'
+    # Bash test: zcat < Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz | awk '!/^#+/' |  awk '$3 ~ /^gene$/ {print $0}' | awk '{sum += $5-$4} END {print sum}'
+    assert answers[1] == '3. Total length of genes: 3,944,686', 'Answer 1 = ' + answers[1] + ', expected = 3. Total length of genes: 3,944,686'
+    # Bash test: zcat < Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz | tail -n 77361 | tr -d '\n' | wc -c
+    assert answers[2] == '4. Total length of genome: 4,641,652', 'Answer 1 = ' + answers[2] + ', expected = 4. Total length of genome: 4,641,652'
+    assert answers[3] == '5. Perecent of genome which is coding: 84.98%', 'Answer 1 = ' + answers[3] + ', expected = 5. Perecent of genome which is coding: 84.98%'
 
 # %%
